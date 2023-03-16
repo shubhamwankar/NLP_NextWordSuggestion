8fcd61db5f8d57af5a3039dd878305755e027c3f
shubhamwankar <shubhamwankar23@gmail.com>
2023-03-17 03:12:52 +0530

    added files

--- load_predict.py
+++ load_predict.py
@@ -1,10 +1,21 @@
 from transformers import pipeline
 import pandas as pd
 
+# Initializing the pipeline
+
 mask_pipeline = pipeline("fill-mask",
                 model='distilbert-base-uncased-finetuned-empathetic-dialogues')
 
+# Predict Function
 def predict(sample=''):
+    """
+    Enter sample text in the format:
+    "I am feeling [MASK]."
+
+    The model will return a dataframe containing the predictions and respective 
+    probability scores for the same.
+    
+    """
     preds = mask_pipeline(sample)
     df = pd.DataFrame(preds)
     df['score'] = df['score'].apply(lambda x: round(x, 2))
@@ -16,6 +27,6 @@ def predict(sample=''):
     print('-'*50)
     return df
 
-sample = 'I [MASK] eating pie!'
+sample = 'I love eating [MASK]!'
 
 predict(sample)
